bceaad4caebff12aa2e00baa19aa1a69a3f0b414
GUaN <152492391+GU7432@users.noreply.github.com>
2025-02-02 08:28:32 +0800

    更新設定間隔時間功能

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import time, random
 import tkinter as tk
 import threading
 from randommessage import getmes
-from controlchrome import send
+from controlchrome import send, init
 
+lo = 1
+hi = 3
 isrunning = False
 def Sending():
     global isrunning
@@ -13,7 +15,8 @@ def Sending():
         while text == pre: text = getmes()
         pre = text
         send(pre)
-        wait = random.uniform(1, 3)
+        wait = random.uniform(lo, hi)
+        print(wait)
         time.sleep(wait)
 
 def startSending():
@@ -26,6 +29,15 @@ def stopSending():
     global isrunning
     isrunning = False
 
+def openurl():
+    url = urlEntry.get()
+    print(url)
+    if url: init(url)
+
+def settiming():
+    global lo, hi
+    lo = int(loEntry.get())
+    hi = int(hiEntry.get())
 
 # 創建主窗口
 root = tk.Tk()
@@ -39,7 +51,27 @@ start_btn.pack(pady=10)
 stop_btn = tk.Button(root, text="停止發送訊息", command=stopSending, width=15, height=2)
 stop_btn.pack(pady=10)
 
-# # 倒數計時
+# 輸入框（Entry）開啟網址按鈕
+urlEntry = tk.Entry(root, width=40)
+urlEntry.pack(pady=10)
+
+openurl_btn = tk.Button(root, text="打開網址", command=openurl)
+openurl_btn.pack(pady=10)
+
+# 設定刷貓間隔
+setlabel = tk.Label(root, text="設定刷貓間隔", font=("Arial", 16))
+setlabel.pack(pady=10)
+
+loEntry = tk.Entry(root, width=5)
+loEntry.pack(pady=10)
+
+hiEntry = tk.Entry(root, width=5)
+hiEntry.pack(pady=10)
+
+settime_btn = tk.Button(root, text="設定", command=settiming)
+settime_btn.pack(pady=10)
+
+# 倒數計時
 # timer_label = tk.Label(root, text="等待開始...", font=("Arial", 16))
 # timer_label.pack(pady=20)
 
