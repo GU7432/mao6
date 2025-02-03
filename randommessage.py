e0e03dfa327f80c4b4e441cfe27b5a628c78c7c6
GUaN <152492391+GU7432@users.noreply.github.com>
2025-02-03 10:56:11 +0800

    Update randommessage.py

--- randommessage.py
+++ randommessage.py
@@ -8,5 +8,4 @@ def getmes():
     n = random.randint(1, 4)
     random_message = ""
     for _ in range(n): random_message += random.choice(messages).rstrip("\n")
-    # print(random_message)
     return random_message
